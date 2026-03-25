d03519e17cb5687221fde5c069af9f9ed90ca3c7
jpre <jpre@itu.dk>
2026-03-25 18:42:14 +0100

    Create TestController.cs
    Added some vulnerabilities to test

---
+++ src/Chirp.Web/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace Chirp.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public string Get(string userInput)
+        {
+            // ❌ INTENTIONALLY VULNERABLE (SQL Injection)
+            string query = "SELECT * FROM Users WHERE Name = '" + userInput + "'";
+
+            using (var connection = new SqlConnection("Server=localhost;Database=Test;User Id=sa;Password=Password123;"))
+            {
+                connection.Open();
+                var command = new SqlCommand(query, connection);
+                var reader = command.ExecuteReader();
+            }
+
+            return "Done";
+        }
+    }
+}
